afce5d74efc830681f8552d460fcb6ec8e35b24d
sanjay <sanjaymalakkal222@gmail.com>
2022-07-06 22:05:55 +0530

    commit2

--- user/views.py
+++ user/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def welcome(request):
+    return render(request,'user/userhome.html')
 
 # Create your views here.
