386f9b02eeeb02e16158501ddc9600d296e87f60
sanjay <sanjaymalakkal222@gmail.com>
2022-07-07 12:43:14 +0530

    commit7

--- manager/views.py
+++ manager/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def managerhome(request):
-    return render(request,'owner/managerhome.html')
+    return render(request,'manager/managerhome.html')
