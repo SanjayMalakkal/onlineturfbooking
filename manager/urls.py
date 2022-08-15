a08fe96272c0401f7aafd18fd2bfb0799b2422ee
sanjay <sanjaymalakkal222@gmail.com>
2022-08-16 00:27:40 +0530

    new7

--- manager/urls.py
+++ manager/urls.py
@@ -3,6 +3,8 @@ from .import views
 
 urlpatterns = [
 
-   path("homepage",views.manager_home,name='manager_home')
+   path("homepage",views.manager_home,name='manager_home'),
+   path("addturf",views.add_turf,name='addturf'),
+   path("updations",views.turf_updations,name='update')
    
 ]
--- manager/views.py
+++ manager/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 def manager_home(request):
     return render(request,'manager/manager_home.html')
 
+
+def add_turf(request):
+    return render(request,'manager/add_turf.html')
+
+
+def turf_updations(request):
+    return render(request,'manager/updations.html')
