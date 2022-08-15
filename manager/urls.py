455f7127d1908478423be44e797eb3dc46bf09ab
sanjay <sanjaymalakkal222@gmail.com>
2022-08-16 00:30:29 +0530

    new8

--- manager/urls.py
+++ manager/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns = [
 
-   path("homepage",views.manager_home,name='manager_home'),
+   path("",views.manager_home,name='manager_home'),
    path("addturf",views.add_turf,name='addturf'),
    path("updations",views.turf_updations,name='update')
    
