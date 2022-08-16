55ef444819f523db03a0df44a904c8bbd0fa4b72
sanjay <sanjaymalakkal222@gmail.com>
2022-08-16 15:24:04 +0530

    newone

--- login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-urlpatterns = [
-  
-   path("login",views.login,name='login'),
-   path("registration_user",views.user_registration,name='user_registration'),
-   path("registration_manager",views.manager_registration,name='manager_registration')
-]
--- login/views.py
+++ login/views.py
@@ -1,15 +1,1 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 
-
-def login(request):
-    return render(request,'login/login_in.html')
-
-
-def user_registration(request):
-    return render(request,'login/registration_user.html')
-
-
-def manager_registration(request):
-    return render(request,'login/registration_manager.html')
